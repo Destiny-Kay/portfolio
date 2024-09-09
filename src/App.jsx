5f8be2c4cb45d0ec8a21553bef03a51b2ea0db04
Destiny-Kay <destinykevogo@gmail.com>
2024-09-09 19:07:39 +0300

    Added more files

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ import arrowIconWhite from './assets/arrow-diagonal-white.svg'
 import arrowDiagonal from './assets/arrow-diagonal.svg'
 import { useRef } from 'react'
 import { Link } from 'react-router-dom'
+import '/veri-doctor-landing.png'
+import '/magic-mailer.png'
+import Footer from './components/Footer'
+import { TbWorldWww } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 
 function App() {
@@ -32,15 +38,23 @@ function App() {
       <p className='gravitas hero-text'>I am Destiny</p>
       <p className='gideon-roman'>A full stack software engineer and designer.</p>
       <div className='button-group flex-col mt-4'>
-        <button className='button button-filled'><Link>chat me <img src={whatsappBlack}></img></Link></button>
-        <button className='button button-outlined button-link'><Link>View resume <img src={arrowIconWhite}></img></Link></button>
+        <div
+          onClick={() => window.open('https://wa.me/+254706917928')}
+          className='z-10'>
+          <button className='button button-filled'>chat me <img src={whatsappBlack}></img></button>
+        </div>
+        <div
+          onClick={() => window.open('https://docs.google.com/document/d/1gmno1JZ_hsTgmIXlM2RXpUQ9uxxYH8D-Ma-OSLqph2E/edit?usp=sharing')}
+          className='z-10'>
+          <button className='button button-outlined button-link'>View resume <img src={arrowIconWhite}></img></button>
+        </div>
       </div>
       <div className='socials'>
         <p>connect with me</p>
         <div className='button-group'>
-          <Link className='cursor-pointer' to={'https://x.com/DEst'}><img src={xGreen}></img></Link>
-          <Link className='cursor-pointer' to={'https://linkedin.com/in/DestinyKevogo'}><img src={linkedinGreen}></img></Link>
-          <Link className='cursor-pointer' to={'https://github.com'}><img src={githubGreen}></img></Link>
+          <Link className='cursor-pointer z-10' to={'https://x.com/Destinykevogo'}><img src={xGreen}></img></Link>
+          <Link className='cursor-pointer z-10' to={'https://www.linkedin.com/in/destinykevogo/'}><img src={linkedinGreen}></img></Link>
+          <Link className='cursor-pointer z-10' to={'https://github.com/Destiny-Kay'}><img src={githubGreen}></img></Link>
         </div>
       </div>
     </section>
@@ -55,14 +69,52 @@ function App() {
 
     <section id='work' ref={workRef}>
       <h1 className='gravitas header'>work</h1>
-      <WorkCard name={'veri doctor'} position={'Full stack software engineer'} timeline={'March 2024 - Present'}/>
-      <WorkCard name={'nanesoft labs'} position={'Full stack software engineer'} timeline={'March 2024 - Present'}/>
-      <WorkCard name={'SOMO'} position={'contract software engineer'} timeline={'March 2024 - Present'}/>
+      <WorkCard name={'veri doctor'} position={'Full stack software engineer (part-time)'} timeline={'March 2024 - Present'}/>
+      <WorkCard name={'nanesoft labs'} position={'Full stack software engineer'} timeline={'Jul 2023 - Jan 2024'}/>
+      <WorkCard name={'SOMO'} position={'contract software engineer'} timeline={'Oct 2023 - Jan 2024'}/>
+      <WorkCard name={'SOMO'} position={'Software engineerming internship'} timeline={'May 2022 - Apr 2023'}/>
     </section>
 
-    <section id='contact' ref={contactRef}>
-      <h1 className='gravitas header'>contact</h1>
+    <section id='projects' ref={contactRef}>
+      <h1 className='gravitas header'>Projects</h1>
+      <div className='projects-grid'>
+        <div>
+          <p className='project-title gravitas'>Veri doctor.</p>
+          <p className='project-role'>Role: Full stack software developer</p>
+          <p className='project-description'>
+              A web application that enables patients to book and manage their appointments with specialised service providers.
+              Provides doctors with an application to manage their schedule and check all appointments on their side.
+          </p>
+          <div className='project-more-info'>
+            <TbWorldWww className='cursor-pointer' onClick={() => {window.open('https://www.veridoctor.com')}}/>
+            <FaGithub />
+          </div>
+        </div>
+        <img src='veri-doctor-landing.png' className='project-image'></img>
+      </div>
+
+      <div className='projects-grid'>
+        <div className=''>
+          <p className='project-title gravitas'>Magic Mailer.</p>
+          <p className='project-role'>Role: Full stack software developer</p>
+          <p className='project-description'>
+            Automates mass email sending process. Provides users with a way to manage their contacts and email groups. Allows mass emails to be sent with 
+            just one click
+          </p>
+          <div className='project-more-info'>
+            <FaYoutube />
+            <FaGithub />
+          </div>
+        </div>
+        <img src='magic-mailer.png' className='project-image col-start-1'></img>
+      </div>
+
     </section>
+
+    {/* <section id='contact'>
+      <h1 className='gravitas header'>Get in touch</h1>
+    </section> */}
+    <Footer/>
   </>
   )
 }
